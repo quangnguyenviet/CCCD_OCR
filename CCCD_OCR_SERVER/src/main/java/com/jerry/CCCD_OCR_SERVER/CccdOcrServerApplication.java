f32680e887070f7c007620f6e8ce1f0900461ce4
quang nguyen <quangjr07@gmail.com>
2026-03-24 06:58:45 +0700

    init

---
+++ CCCD_OCR_SERVER/src/main/java/com/jerry/CCCD_OCR_SERVER/CccdOcrServerApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.CCCD_OCR_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccdOcrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccdOcrServerApplication.class, args);
+	}
+
+}
---
+++ CCCD_OCR_SERVER/src/test/java/com/jerry/CCCD_OCR_SERVER/CccdOcrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.CCCD_OCR_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccdOcrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
